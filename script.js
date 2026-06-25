7cc6ddbadfb4b0ffe55891002e8de0e7994d5c9d
juliaoliveiraxavier-tech <julia.oliveira.xavier@escola.pr.gov.br>
2026-06-25 10:45:32 -0300

    2

--- script.js
+++ script.js
@@ -1,88 +1,55 @@
 const counters = document.querySelectorAll(".counter");
 
+counters.forEach(counter => {
+
+let target = +counter.dataset.target;
+let count = 0;
+
+let update = () => {
+
+count += target / 100;
+
+if(count < target){
+counter.innerText = Math.floor(count);
+requestAnimationFrame(update);
+}else{
+counter.innerText = target;
+}
+};
+
+update();
+});
+
 const observer = new IntersectionObserver(entries => {
 
 entries.forEach(entry => {
 
 if(entry.isIntersecting){
 
-const counter = entry.target;
-const target = Number(counter.dataset.target);
+entry.target.classList.add("active");
 
-let count = 0;
-
-const update = () => {
-
-count += target / 100;
-
-if(count < target){
-
-counter.innerText = Math.floor(count);
-requestAnimationFrame(update);
-
-}else{
-
-counter.innerText = target;
 }
-};
 
-update();
-
-observer.unobserve(counter);
-}
-});
 });
 
-counters.forEach(counter=>{
-observer.observe(counter);
 });
-
-const darkBtn = document.getElementById("darkToggle");
-
-darkBtn.addEventListener("click",()=>{
-
-document.body.classList.toggle("dark");
-
-if(document.body.classList.contains("dark")){
-darkBtn.innerHTML =
-'<i class="fas fa-sun"></i>';
-}else{
-darkBtn.innerHTML =
-'<i class="fas fa-moon"></i>';
-}
-});
-
-for(let i=0;i<40;i++){
-
-const particle =
-document.createElement("div");
-
-particle.classList.add("circle");
-
-let size = Math.random()*25+10;
-
-particle.style.width=size+"px";
-particle.style.height=size+"px";
-
-particle.style.left=
-Math.random()*100+"%";
-
-particle.style.animationDuration=
-(10+Math.random()*15)+"s";
 
 document
-.getElementById("particles")
-.appendChild(particle);
-}
+.querySelectorAll("section")
+.forEach(section => {
+
+section.classList.add("reveal");
+
+observer.observe(section);
+
+});
 
 document
-.getElementById("formulario")
-.addEventListener("submit",e=>{
+.getElementById("contactForm")
+.addEventListener("submit", e => {
 
 e.preventDefault();
 
-alert(
-"Obrigado por apoiar a sustentabilidade 🌱"
-);
+alert("Mensagem enviada com sucesso! 🌱");
 
-}); v
+});
